de02b7516a79e066f9b01dfe55b3e7d14985c2fb
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2017-06-15 13:34:10 -0400

    update

--- write_config-mutect2.py
+++ write_config-mutect2.py
@@ -4,6 +4,15 @@ import glob
 import argparse
 import csv
 
+class BAM():
+	def __init__(self,name,bam):
+		self.id=name
+		if bam:
+			self.bam=bam
+		elif os.path.isfile('bam_input/final/{0}.ready.bam'.format(self.id)):
+			self.bam=os.path.abspath('bam_input/final/{0}.ready.bam'.format(self.id))
+		else:
+			raise OSError('No file found: {0}\nTry including full path to bam in input file'.format(os.path.abspath('bam_input/final/{0}.ready.bam'.format(self.id))))
 
 def mkdir_p(path):
 	try:
@@ -12,50 +21,51 @@ def mkdir_p(path):
 		if exc.errno == errno.EEXIST and os.path.isdir(path):
 			pass
 
-def write_script(tumor,normal,x,y):
-	tbam='/media/Nandi/BAM_FILES/MSKCC/'+x
-	nbam='/media/Nandi/BAM_FILES/MSKCC/'+y
-	#for dir in ['BRCA','MORE_BRCA','MIXED']:
-	#	if os.path.isfile('{0}/{1}.bam'.format(dir,tumor)):
-	#		tbam=os.path.abspath('{0}/{1}.bam'.format(dir,tumor))
-	#	if os.path.isfile('{0}/{1}.bam'.format(dir,normal)):
-	#		nbam=os.path.abspath('{0}/{1}.bam'.format(dir,normal))
-	with open('mutect/{0}/mutect.sh'.format(tumor), 'wb') as script:
+def write_script(t,n):
+	with open('data/final/{0}/mutect2.sh'.format(t.id),'wb') as script:
 		script.write('#!/bin/bash\n\n')
-		script.write('SCRIPT=$(readlink -f "$0")\n')
-		script.write('cd $(dirname "$SCRIPT")\n\n')
-		script.write('ref="/media/Shakti/Genomes/Human/b37_genomes/human_g1k_v37.fasta"\n')
-		script.write('dbsnp="/media/Shakti/Genomes/Human/b37_genomes/dbsnp_137.b37.vcf"\n')
-		script.write('cosmic="/media/Shakti/Genomes/Human/b37_genomes/CosmicCodingMuts_v69_b37.vcf"\n\n')
-		script.write('java -Xmx100g -jar /software/$GATK/GenomeAnalysisTK.jar \\\n')
-		script.write('-T MuTect \\\n')
+		script.write('module load java-sdk-1.8.0\n\n')
+		#script.write('SCRIPT=$(readlink -f "$0")\n')
+		#script.write('cd $(dirname "$SCRIPT")\n\n')
+		script.write('ref=""\n')
+		script.write('dbsnp=""\n')
+		script.write('cosmic=""\n')
+		script.write('GATK="/home/bwubb/software/GenomeAnalysisTK-3.7"\n\n'
+		script.write('java -Xmx30g -jar /software/$GATK/GenomeAnalysisTK.jar \\\n')
+		script.write('-T MuTect2 \\\n')
 		script.write('-R $ref \\\n')
 		script.write('--dbsnp $dbsnp \\\n')
 		script.write('--cosmic $cosmic \\\n')
 		#script.write('--intervals {} \\\n'.format(intervals_file))
-		script.write('--input_file:normal {} \\\n'.format(nbam))
-		script.write('--input_file:tumor {} \\\n'.format(tbam))
-		script.write('--out {}.call_stats.txt \\\n'.format(tumor))
-		script.write('--coverage_file {}.coverage.wig.txt \\\n'.format(tumor))
-		script.write('--vcf {}.mutect.vcf\n'.format(tumor))
-	print 'mutect/{}/mutect.sh was written.'.format(tumor)
+		script.write('--input_file:normal {} \\\n'.format(n.bam))
+		script.write('--input_file:tumor {} \\\n'.format(t.bam))
+		script.write('--out {}.call_stats.txt \\\n'.format(t.id))
+		script.write('--coverage_file {}.coverage.wig.txt \\\n'.format(t.id))
+		script.write('--vcf {0}\n'.format(os.path.abspath('data/final/{0}/{0}.mutect.vcf\n'.format(t.id))))
+	print 'data/config/{0}/mutect.sh was written.'.format(t.id)
 
 def make_dirs(tumor,normal,):
 	mkdir_p('mutect/{}'.format(tumor))
 
 def main(argv=None):
 	p = argparse.ArgumentParser()
-	p.add_argument('-i', '--infile', help='Tab-delimited file; Tumor Normal pair IDs')
+	p.add_argument('-i','--infile',help='Tab-delimited file; Tumor Normal pair IDs')
+	p.add_argument('-L','--intervals',help='')
+	#p.add_argument('--gatk',help='Specify a specific GATK version (if it exists in $HOME/software/)')
+	#p.add_argument('--build',help='')#Add json reading for files of versions
 	args = p.parse_args()
 	for a, b in vars(args).items():
 		print '{}: {}'.format(a,b)
 	
 	with open(args.infile, 'rb') as file:
-		pairs = [line.split('\t') for line in file.read().splitlines()]
-		mkdir_p('mutect')
-		for pair in pairs:
-			mkdir_p('mutect/{0}'.format(pair[0]))
-			write_script(*pair[:4 ])
+		reader=csv.DictReader(file,delimiter='\t',fieldnames=['t_id','n_id','t_bam','n_bam'],restval=False)
+		for row in reader:
+			if any(v=k for k,v in row.items()):
+				continue
+			t=BAM(row['t_id'],row['t_bam'])
+			n=BAM(row['n_id'],row['n_bam'])
+			write_script(t,n)
+			
 
 
 if __name__ =='__main__':

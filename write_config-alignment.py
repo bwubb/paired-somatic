4d9e75fa9c48163e527dd3807801d2f33c6004ea
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2017-06-22 14:52:20 -0400

    update

--- write_config-alignment.py
+++ write_config-alignment.py
@@ -31,14 +31,12 @@ def ref_by_build(build):
 	REF={'hg19':'','GRCh37':'/home/bwubb/resources/Genomes/Human/GRCh37/human_g1k_v37.fasta','hg38':'','GRCh38':'','GRCh38_GDC':'/home/bwubb/GRCh38_GDC/GRCh38.d1.vd1.fa'}
 	return REF[build]
 
-def write_script(sample,fqs,LB,ref,no_merge):#Project?
+def write_script(sample,fqs,LB,ref):#Project?
 	with open('bam_input/config/{0}/aln.sh'.format(sample),'wb') as script:
 		script.write('#!/bin/bash\n\n')
 		script.write('ref="{0}"\n'.format(ref))#standardize?
 		#script.write('PICARD=\n\n')
 		script.write('module load picard-1.96\n\n')
-		#script.write('SCRIPT=$(readlink -f "$0")\n')
-		#script.write('cd $(dirname "$SCRIPT")\n\n')
 		for i,j in enumerate(range(1,len(fqs),2)):
 			ID,PU=_get_ID_PU(fqs[j-1])
 			script.write('bwa mem -M -t 16 $ref {0} {1} | samtools view -bS - > {2}.bam\n'.format(fqs[j-1],fqs[j],ID))
@@ -59,14 +57,8 @@ def write_script(sample,fqs,LB,ref,no_merge):#Project?
 			script.write('samtools merge -f {0} *COsorted.wRG.bam\n\n'.format(bam))
 		else:
 			script.write('mv {0}.COsorted.wRG.bam {1}\n\n'.format(ID,bam))
-		
-		
-		
-		#Add verbosity option, or just always have it in output
-		#script.write('samtools index {0}\n\n'.format(bam))
 		script.write('if [ -e {0} ]; then rm *.COsorted*; fi\n\n'.format(bam))
-		#script.write('samtools view -h {0} | samblaster -M | samtools view -Sb - > {1}\n'.format(bam,)#more bam names
-		
+
 
 def get_args():
 	'''Parse sys.argv'''
@@ -75,7 +67,7 @@ def get_args():
 	parser.add_argument('--fastq-dir',default='FASTQ',help='Directory where fastq files may be found.') #Display default
 	parser.add_argument('--lib',default='unknown',help='Name associated with capture library. Will become LB tag')
 	parser.add_argument('--build',default='GRCh37',help='')
-	parser.add_argument('--no-merge',action='store_true',default=False,help='Do no merge')
+	#parser.add_argument('--no-merge',action='store_true',default=False,help='Do no merge')
 	#parser.add_argument('-w','--template',required=True,help='Name of bcbio template')
 	#parser.add_argument('-r','--resource_file',help='resource file to include')
 	return parser.parse_args()
@@ -98,7 +90,7 @@ def main(argv=None):
 		
 		ref=ref_by_build(args.build)
 		if ref:
-			write_script(sample,fqs,args.lib,ref,args.no_merge)
+			write_script(sample,fqs,args.lib,ref)
 			print sample
 #		if check:
 #			write_yaml(sample,fqs)

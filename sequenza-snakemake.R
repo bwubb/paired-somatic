f5dd79b3d28e44a913c87da4e9c2bc5e4e10a0ca
Brad <WubbenhB@pmacs.upenn.edu>
2018-06-20 11:35:02 -0400

    Update
    Update

---
+++ sequenza-snakemake.R
@@ -0,0 +1,6 @@
+
+
+library(sequenza)
+tumor<-sequenza.extract(snakemake@input[[1]])
+CP<-sequenza.fit(tumor,mc.cores=snakemake@threads)
+sequenza.results(sequenza.extract=tumor,cp.table=CP,sample.id=snakemake@wildcards[["tumor"]],out.dir=snakemake@params[["outdir"]])
